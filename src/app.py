8a140650acf0266c22696f36ed5e0309393f2160
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-17 17:12:20 -0400

    subindo server

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from src.routes import routes_bp
+
+app = Flask(__name__)
+
+CORS(app)
+app.register_blueprint(routes_bp)
+
+if __name__ == "__main__":
+    app.run(port=5000, host='localhost', debug=True)
