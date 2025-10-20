77da4f785004b21979334f68c2290813a21139bc
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-20 16:32:10 -0400

    cadastrando ponto de descarte

--- src/controllers/cadastrar.py
+++ src/controllers/cadastrar.py
@@ -1,12 +1,40 @@
 from flask import jsonify, make_response, request
+import mysql.connector
+from db_config.db_conection import create_conection
 
 def cadastrar():
-    data = request.form.to_dict()
-    print(data.get("nome"))
-    print(data.get("cnpj"))
-    print(data.get("estado"))
-    print(data.get("cidade"))
-    print(data.get("endereco"))
-    print(data.get("numero"))
-    
-    return jsonify({"msg": "cheguei"})
+    try:
+        conection = create_conection()
+        
+        if conection:
+            cursor = conection.cursor()
+
+            data = request.form.to_dict()
+            
+            nome = data.get("nome")
+            cnpj = data.get("cnpj")
+            estado = data.get("estado")
+            cidade = data.get("cidade")
+            endereco = data.get("endereco")
+            numero = data.get("numero")
+            
+            insert_query = """
+                INSERT INTO pontos (nome, cnpj, estado, cidade, endereco, numero)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """
+            values = (nome, cnpj, estado, cidade, endereco, numero)
+
+            cursor.execute(insert_query, values)
+            conection.commit()
+
+            return jsonify({"message": "Ponto de descarte cadastrado com sucesso!"}), 201
+
+    except mysql.connector.Error as err:
+        return jsonify({
+            "message": f"Não foi possivel cadastrar o ponto de descarte! Erro: {err}"
+        })
+    finally:
+        if cursor:
+            cursor.close()
+        if conection:
+            conection.close()
