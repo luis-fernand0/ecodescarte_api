233002ec2798591fe269a17eb16d462cf932f017
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-20 16:31:39 -0400

    melhorando tratamento de erros e separação de responsabilidades

--- src/db_config/db_conection.py
+++ src/db_config/db_conection.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
 import mysql.connector
+from mysql.connector import Error
 
 load_dotenv()
 
-db_host = os.getenv("HOST")
-db_port = os.getenv("PORTA")
-db_user = os.getenv("USER")
-db_pass = os.getenv("PASS")
+def create_conection():
+    db_host = os.getenv("HOST")
+    db_port = os.getenv("PORTA")
+    db_user = os.getenv("USER")
+    db_pass = os.getenv("PASS")
+    db = os.getenv("DATABASE")
 
-conection = mysql.connector(
-    host=db_host,
-    user=db_user,
-    password=db_pass
-)
+    try:
+        conection = mysql.connector.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            database=db,
+            password=db_pass
+        )
+        return conection
+    except Error:
+        print(f"Não foi possivel se conectar ao banco, erro: {Error}")
+        return None
