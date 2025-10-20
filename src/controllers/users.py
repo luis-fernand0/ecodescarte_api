36bb221d2d5bcb1f98ba7f40b3086639c786c8ef
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-20 16:33:37 -0400

    buscando cidade do cep enviado na req e buscando no banco de dados os pontos de descarte mais proximos

--- src/controllers/users.py
+++ src/controllers/users.py
@@ -1,26 +1,40 @@
-from flask import jsonify, make_response
-
-pontos = [
-    {
-        'id': 1,
-        'nome': 'Centro de reciclagem 1',
-        'endereco': 'Rua exemplo, jardim exemplo, 000, EXEMPLO-EX',
-        'funcionamento': 'Seg a Sab'
-    },
-    {
-        'id': 2,
-        'nome': 'Centro de reciclagem 2',
-        'endereco': 'Rua exemplo, jardim exemplo, 000, EXEMPLO-EX',
-        'funcionamento': 'Seg a Sex'
-    },
-    {
-        'id': 3,
-        'nome': 'Centro de reciclagem 3',
-        'endereco': 'Rua exemplo, jardim exemplo, 000, EXEMPLO-EX',
-        'funcionamento': 'Seg a Dom'
-    }
-]
+from flask import jsonify, make_response, request
+import requests
+import mysql.connector
+from db_config.db_conection import create_conection
 
 def get_users():
-    response = make_response(jsonify(pontos), 200)
-    return response
+    try:
+        cep = request.args.get('cep')
+        cep = cep.replace("-", "")
+
+        url = f"https://brasilapi.com.br/api/cep/v1/{cep}"
+
+        response = requests.get(url)
+        dados = response.json()
+
+        conection = create_conection()
+
+        if not conection:
+            return jsonify({"message": "Não foi possivel se conectar com o banco"}), 500
+        
+        cursor = conection.cursor()
+
+        select_query = """
+            SELECT * FROM pontos WHERE cidade = %s
+        """
+        values = dados["city"]
+
+        cursor.execute(select_query, (values,))
+        resultados = cursor.fetchall()
+
+        print(resultados)
+
+        return jsonify({"message": "Cheguei aqui"}), 200
+    except:
+        return jsonify({"message": "Não foi possivel buscar os pontos de descarte"}), 500
+    finally:
+        if cursor:
+            cursor.close()
+        if conection:
+            conection.close()
