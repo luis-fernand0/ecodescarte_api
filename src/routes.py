662d782283a2089318ed90b2806a1a57df2a9c53
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-21 16:53:02 -0400

    mudando nome da rota /users para /pontos

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
-from controllers.users import get_users
+from controllers.pontos import get_pontos
 from controllers.cadastrar import cadastrar
 
 routes_bp = Blueprint("routes", __name__)
 
-routes_bp.route("/users", methods=["GET"])(get_users)
+routes_bp.route("/pontos", methods=["GET"])(get_pontos)
 routes_bp.route("/cadastrar", methods=["POST"])(cadastrar)
