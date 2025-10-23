1d9e5b7a13a02686a108718cee1015384039d7fa
luis-fernando-job <fernandoluis270@gmail.com>
2025-10-23 10:50:17 -0400

    removendo importações desnecessarias e removendo trecho que fazia req para a api do cep

--- src/controllers/pontos.py
+++ src/controllers/pontos.py
@@ -1,6 +1,5 @@
-from flask import jsonify, make_response, request
-import requests
-import mysql.connector
+from flask import jsonify, request
+from utils.buscar_cep import buscar_cep
 from db_config.db_conection import create_conection
 
 def get_pontos():
@@ -13,30 +12,33 @@ def get_pontos():
         cep = request.args.get('cep')
         cep = cep.replace("-", "")
 
-        url = f"https://brasilapi.com.br/api/cep/v1/{cep}"
-        response = requests.get(url)
-        dados = response.json()
+        dados = buscar_cep(cep)
 
-        if response.status_code != 200:
-            return jsonify ({
-                "message": f"Não encontramos nenhuma correspondecia com o CEP informado! {dados["message"]}",
-                "erro": dados["type"] 
-                }), response.status_code
-
+        if not dados['success']:
+            return jsonify(dados)
+        
+        cidade = ''
+        if 'city' in dados['data']:
+            cidade = dados['data']['city']
+        else:
+            cidade = dados['data']['localidade']
+            
         select_query = """
             SELECT * FROM pontos WHERE cidade = %s
         """
-        values = dados["city"]
-
-        cursor.execute(select_query, (values,))
+        
+        cursor.execute(select_query, (cidade,))
         resultados = cursor.fetchall()
 
         if len(resultados) == 0:
             return jsonify({"data": [], "message": "Não encontramos nenhum ponto de descarte proximo ao CEP informado"}), 200
 
         return jsonify({"data": resultados}), 200
-    except:
-        return jsonify({"message": "Não foi possivel buscar os pontos de descarte"}), 500
+    except Exception as err:
+        return jsonify({
+            "data": [],
+            "message": f"Não foi possivel buscar os pontos de descarte! Erro: {err}"
+            }), 500
     finally:
         if cursor:
             cursor.close()
